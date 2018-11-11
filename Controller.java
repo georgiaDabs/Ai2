cc16729471760a0f6e62a2555375bf633ba7b508
georgiaDabs <34480840+georgiaDabs@users.noreply.github.com>
2018-11-11 10:37:24 +0000

    sanity check

--- Controller.java
+++ Controller.java
@@ -2,10 +2,10 @@
 public class Controller
 {
    public Controller(){
-       
-       Search s1=new Search("J:/AIsearchJava-master/AISearchtestcase.txt",500);
-       Search s2=new Search("J:/AIsearchJava-master/AISearchtestcase.txt",500);
-       Search s3=new Search("J:/AIsearchJava-master/AISearchtestcase.txt",500);
+       String link="J:/SearchFiles/SearchFile26.txt";
+       Search s1=new Search(link,500);
+       Search s2=new Search(link,500);
+       Search s3=new Search(link,500);
        Individual best1=s1.getBest();
        int bestCost1=s1.getBestCost();
        Individual best2=s2.getBest();
@@ -13,14 +13,15 @@ public class Controller
        Individual best3=s3.getBest();
        int bestCost3=s3.getBestCost();
        System.out.println("-----------CONTROLLER--------------");
+       System.out.println("link------------->"+link);
        System.out.println("best Tour 1");
        best1.printTour();
        System.out.println("best cost1:"+bestCost1);
        System.out.println("best Tour 2");
        best2.printTour();
-       System.out.println("best cost2:"+bestCost1);
-       System.out.println("best Tour 2");
-       best2.printTour();
-       System.out.println("best cost1:"+bestCost2);
+       System.out.println("best cost2:"+bestCost2);
+       System.out.println("best Tour 3");
+       best3.printTour();
+       System.out.println("best cost1:"+bestCost3);
     }
 }
